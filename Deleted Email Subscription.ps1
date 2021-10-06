6a6418b230d99c043a0275999dee99134304af9c
iqbalkhanmsft <88405519+iqbalkhanmsft@users.noreply.github.com>
2021-10-06 18:05:44 -0400

    Add files via upload

---
+++ Deleted Email Subscription.ps1
@@ -0,0 +1,10 @@
+﻿Login-PowerBI
+
+#Change date range here.
+$activities = Get-PowerBIActivityEvent -StartDateTime '2021-09-28T00:00:00' -EndDateTime '2021-09-28T23:59:59' -ActivityType 'DeleteEmailSubscription' | ConvertFrom-Json
+
+$activities.Count
+$activities[0]
+
+#Change file name here.
+$activities | Export-Csv "C:\Users\Iqbal Khan\Desktop\PBISubscriptionsActivityPowershell - Deleted.csv"
